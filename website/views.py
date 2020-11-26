3e21762d9f4b46770710f6eae7bc8ed601d7fdf9
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-26 14:37:48 +0100

    templates and static files

--- website/views.py
+++ website/views.py
@@ -4,17 +4,113 @@ def home(request):
     return render(request, 'home.html', {})
 
 def add(request):
+    from random import randint
+
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
     if request.method == "POST":
         answer = request.POST['answer']
-        return render(request, 'add.html', {'answer': answer})
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
 
-    return render(request, 'add.html', {})
+        if not answer:
+            my_answer = "Hey! You forgot to fill out the form!"
+            color = 'warning'
+            return render(request, 'divide.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+        correct_answer = int(old_num_1) + int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + ' + ' + old_num_2 + ' = ' + answer
+            color = "success"
+        else:
+            my_answer = "incorrect! " + old_num_1 + ' + ' + old_num_2 + ' is not ' + answer + ' It is ' + str(correct_answer)
+            color = "danger"
+
+        return render(request, 'add.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+    return render(request, 'add.html', {'num_1': num_1, 'num_2': num_2})
 
 def subtract(request):
-    return render(request, 'subtract.html', {})
+    from random import randint
+
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        if not answer:
+            my_answer = "Hey! You forgot to fill out the form!"
+            color = 'warning'
+            return render(request, 'divide.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+        correct_answer = int(old_num_1) - int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + ' - ' + old_num_2 + ' = ' + answer
+            color = "success"
+        else:
+            my_answer = "incorrect! " + old_num_1 + ' - ' + old_num_2 + ' is not ' + answer + ' It is ' + str(correct_answer)
+            color = "danger"
+
+        return render(request, 'subtract.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+    return render(request, 'subtract.html', {'num_1': num_1, 'num_2': num_2})
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+    from random import randint
+
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        if not answer:
+            my_answer = "Hey! You forgot to fill out the form!"
+            color = 'warning'
+            return render(request, 'divide.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+        correct_answer = int(old_num_1) * int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + ' x ' + old_num_2 + ' = ' + answer
+            color = "success"
+        else:
+            my_answer = "incorrect! " + old_num_1 + ' x ' + old_num_2 + ' is not ' + answer + ' It is ' + str(correct_answer)
+            color = "danger"
+
+        return render(request, 'multiply.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+    return render(request, 'multiply.html', {'num_1': num_1, 'num_2': num_2})
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    from random import randint
+
+    num_1 = randint(0, 10)
+    num_2 = randint(1, 10)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        if not answer:
+            my_answer = "Hey! You forgot to fill out the form!"
+            color = 'warning'
+            return render(request, 'divide.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+        correct_answer = int(old_num_1) / int(old_num_2)
+        if float(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + ' / ' + old_num_2 + ' = ' + answer
+            color = "success"
+        else:
+            my_answer = "incorrect! " + old_num_1 + ' / ' + old_num_2 + ' is not ' + answer + ' It is ' + str(correct_answer)
+            color = "danger"
+
+        return render(request, 'divide.html', {'answer': answer, 'my_answer': my_answer, 'num_1': num_1, 'num_2': num_2, 'color': color})
+
+    return render(request, 'divide.html', {'num_1': num_1, 'num_2': num_2})
